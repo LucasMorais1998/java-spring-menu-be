a10e4036e9f18137265ef1f1784e1292fec05a38
Lucas Morais <lucas14.morais@gmail.com>
2023-12-11 22:30:33 -0300

    chore: initialize project

---
+++ src/main/java/com/youtube/springmenuappbe/SpringMenuAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.springmenuappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMenuAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMenuAppBeApplication.class, args);
+	}
+
+}
