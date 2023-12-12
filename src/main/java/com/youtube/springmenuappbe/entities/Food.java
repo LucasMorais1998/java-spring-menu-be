262cea614a6621410d5fb9078364342722324a52
Lucas Morais <lucas14.morais@gmail.com>
2023-12-11 23:05:00 -0300

    feat(entity): add Food entity

---
+++ src/main/java/com/youtube/springmenuappbe/entities/Food.java
@@ -0,0 +1,20 @@
+package com.youtube.springmenuappbe.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+}
